cabf35e8de0f10d0b1f8d2f6ba4ca79f855819d0
reva2080 <3235109770@qq.com>
2023-12-09 15:37:34 +0800

    Site updated: 2023-12-09 15:37:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/09/hello-world/","2023/11/13/“第一篇文章”/"];function toRandomPost(){
+var posts=["2023/12/09/hello-world/","2023/11/13/“第一篇文章”/","2023/12/09/可以访问了！/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
