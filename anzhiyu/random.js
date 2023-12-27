cacd5ae1ef24ddb68747e6f6bd3bb0631cc95d51
reva2080 <3235109770@qq.com>
2023-12-27 13:25:11 +0800

    Site updated: 2023-12-27 13:25:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/09/hello-world/","2023/11/13/“第一篇文章”/","2023/12/09/可以访问了！/"];function toRandomPost(){
+var posts=["2023/12/27/2023年ReVa博客总结/","2023/12/09/hello-world/","2023/11/13/“第一篇文章”/","2023/12/09/可以访问了！/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
